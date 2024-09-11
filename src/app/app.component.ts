f7855a6d16a319aeae3eb7790163a96d51197fd2
angelporlan <angeelporlan4@gmail.com>
2024-09-11 17:58:25 +0200

    add to bag component desktop screen finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { ProfileCardComponent } from './shared/components/profile-card/profile-card.component';
+import { AddToBagComponent } from './shared/components/add-to-bag/add-to-bag.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProfileCardComponent],
+  imports: [CommonModule, RouterOutlet, AddToBagComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/components/add-to-bag/add-to-bag.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-bag',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-to-bag.component.html',
+  styleUrl: './add-to-bag.component.css'
+})
+export class AddToBagComponent {
+
+}
